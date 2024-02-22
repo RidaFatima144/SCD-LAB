583c83d8eb176cb39e8f0ad561c36c9b71448543
RidaFatima144 <158420160+RidaFatima144@users.noreply.github.com>
2024-02-22 09:29:32 +0500

    Update shapes.py

--- shapes.py
+++ shapes.py
@@ -1,1 +1,83 @@
+import math
 
+class Shape:
+    def area(self):
+        pass
+
+    def perimeter(self):
+        pass
+
+
+class Rectangle(Shape):
+    def _init_(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return 2 * (self.width + self.height)
+
+
+class Circle(Shape):
+    def _init_(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * self.radius**2
+
+    def perimeter(self):
+        return 2 * math.pi * self.radius
+
+
+Q2.2
+import math
+
+class Shape:
+    def area(self):
+        pass
+
+    def perimeter(self):
+        pass
+
+
+class Rectangle(Shape):
+    def _init_(self, width, height):
+        self.__width = width
+        self.__height = height
+
+    def get_width(self):
+        return self.__width
+
+    def set_width(self, width):
+        self.__width = width
+
+    def get_height(self):
+        return self.__height
+
+    def set_height(self, height):
+        self.__height = height
+
+    def area(self):
+        return self._width * self._height
+
+    def perimeter(self):
+        return 2 * (self._width + self._height)
+
+
+class Circle(Shape):
+    def _init_(self, radius):
+        self.__radius = radius
+
+    def get_radius(self):
+        return self.__radius
+
+    def set_radius(self, radius):
+        self.__radius = radius
+
+    def area(self):
+        return math.pi * self.__radius ** 2
+
+    def perimeter(self):
+        return 2 * math.pi * self.__radius
