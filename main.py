75762242f067cf8bb4c06db8bd61e271d5c42d7b
RidaFatima144 <158420160+RidaFatima144@users.noreply.github.com>
2024-02-22 09:46:59 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+# Importing the Rectangle and Circle classes from shapes.py
+from shapes import Rectangle, Circle
 
+# Instantiating objects of Rectangle and Circle
+rectangle = Rectangle(4, 5)
+circle = Circle(3)
+
+print("Rectangle Area:", rectangle.area())
+print("Rectangle Perimeter:", rectangle.perimeter())
+
+print("Circle Area:", circle.area())
+print("Circle Circumference:", circle.perimeter())
