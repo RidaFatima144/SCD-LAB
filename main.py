93e11d5737a309c3293d0855c5ad205112fb504b
RidaFatima144 <158420160+RidaFatima144@users.noreply.github.com>
2024-02-22 10:14:48 +0500

    Update main.py

--- main.py
+++ main.py
@@ -10,3 +10,49 @@ print("Rectangle Perimeter:", rectangle.perimeter())
 
 print("Circle Area:", circle.area())
 print("Circle Circumference:", circle.perimeter())
+
+def print_details(shape):
+    print("Area:", shape.area())
+    print("Perimeter:", shape.perimeter())
+
+rectangle = Rectangle(4, 5)
+circle = Circle(3)
+
+print("Rectangle Details:")
+print_details(rectangle)
+
+print("Circle Details:")
+print_details(circle)
+
+q 3.4
+def print_details(shape):
+    print("Area:", shape.area())
+    print("Perimeter:", shape.perimeter())
+
+# Creating instances of Rectangle and Circle
+rectangle = Rectangle(4, 5)
+circle = Circle(3)
+
+# Calling print_details with instances of Rectangle and Circle
+print("Rectangle Details:")
+print_details(rectangle)
+
+print("Circle Details:")
+print_details(circle)
+
+q3.5 
+def print_details(shape):
+    print("Details of the shape:")
+    print("Area:", shape.area())
+    print("Perimeter:", shape.perimeter())
+
+# Creating instances of Rectangle and Circle
+rectangle = Rectangle(4, 5)
+circle = Circle(3)
+
+# Calling print_details with instances of Rectangle and Circle
+print("Rectangle Details:")
+print_details(rectangle)
+
+print("\nCircle Details:")
+print_details(circle)
